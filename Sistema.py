28190598f57d2561670ce0f6e951b2d05bbcb8d3
Your Name <you@example.com>
2024-06-03 14:32:03 +0100

    Funciona la interfaz grafica de test

--- Sistema.py
+++ Sistema.py
@@ -1,6 +1,9 @@
 from enum import Enum
 import pygame
 from time import sleep
+import tkinter as tk
+from tkinter import messagebox, ttk
+from tkinter import *
 
 class Sonidos(Enum):
     HIMNO_URSS = "Sonidos/Himno nacional de la Unión de Repúblicas Socialistas Soviéticas.mp3"
@@ -20,6 +23,21 @@ def detenerSonidos():
 
 def delay(segundos):
     sleep(segundos)
+    
+sistema = Sistema()
+    
+root = tk.Tk()
+root.title("Escape room")
+
+juegoAnterior = ttk.Button(root, text="Botón 1 (A)", command=sistema.nivelAnterior)
+juegoAnterior.grid(row=0, column=0, padx=10, pady=10)
+root.bind("<KeyPress-Left>", lambda e: sistema.nivelAnterior())
+
+juegoSiguiente = ttk.Button(root, text="Botón 2 (B)", command=sistema.nivelAnterior)
+juegoSiguiente.grid(row=0, column=1, padx=10, pady=10)
+root.bind("<KeyPress-Right>", lambda e: sistema.nivelAnterior())
+
+def siguienteJuego
 
 class Sistema:
     niveles = []#Agregá niveles utilizando las clases hijas -> [Nivle1(), Nivle2(), Nivel3("qwerty"), ...]
--- testInterfazGrafica.py
+++ testInterfazGrafica.py
@@ -1,13 +1,22 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
 from tkinter import *
+from time import sleep
+
+def delay(segundos):
+    sleep(segundos)
 
 # Funciones para los botones
 def accion1():
-    messagebox.showinfo("Acción 1", "¡Botón 1 presionado!")
+    #messagebox.showinfo("Acción 1", "¡Botón 1 presionado!")
+    boton1.grid_forget()
+    boton2.grid()
+    
 
 def accion2():
-    messagebox.showinfo("Acción 2", "¡Botón 2 presionado!")
+    #messagebox.showinfo("Acción 2", "¡Botón 2 presionado!")
+    boton2.grid_forget()
+    boton1.grid()
 
 # Crear la ventana principal
 root = tk.Tk()
@@ -22,8 +31,8 @@ boton1.grid(row=0, column=0, padx=10, pady=10)
 boton2.grid(row=0, column=1, padx=10, pady=10)
 
 # Asociar teclas de atajo
-root.bind("<KeyPress-a>", lambda e: accion1())
-root.bind("<KeyPress-b>", lambda e: accion2())
+root.bind("<KeyPress-Left>", lambda e: accion1())
+root.bind("<KeyPress-Right>", lambda e: accion2())
 
 # Iniciar la aplicación
 root.mainloop()
