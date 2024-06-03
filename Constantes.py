1a5f64aa630581deb019c33d9c114eb5f18f7024
TodorovManu <alumno24.todorov.manuel@ipm.edu.ar>
2024-06-03 08:27:57 -0300

    Funcionan los sonidos

--- Constantes.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class Sonidos(Enum):
-    HIMNO_URSS = "Sonidos/Himno nacional de la Unión de Repúblicas Socialistas Soviéticas.mp3"
-    JIJIJIJA = "Sonidos/JIJIJIJA.mp3"
--- Sistema.py
+++ Sistema.py
@@ -1,17 +1,35 @@
-import Constantes
-from Constantes import Sonidos
-import MetodosComunes
+from enum import Enum
+import pygame
+from time import sleep
+
+class Sonidos(Enum):
+    HIMNO_URSS = "Sonidos/Himno nacional de la Unión de Repúblicas Socialistas Soviéticas.mp3"
+    JIJIJIJA = "Sonidos/JIJIJIJA.mp3"
+
+pygame.mixer.init()
+pygame.mixer.set_num_channels(10)#Cambiar el numero una vez que sepamos cunatos canales vamos a necesitar
+
+def reproducirSonido(sonido, loop, canal):#enum sonido, bool loop, int canal
+    if loop:
+        pygame.mixer.Channel(canal).play(pygame.mixer.Sound(sonido.value), -1)
+    else:
+        pygame.mixer.Channel().play(pygame.mixer.Sound(sonido.value), -1)
+
+def detenerSonidos():
+    pygame.mixer.music.stop()
+
+def delay(segundos):
+    sleep(segundos)
 
 class Sistema:
     niveles = []#Agregá niveles utilizando las clases hijas -> [Nivle1(), Nivle2(), Nivel3("qwerty"), ...]
     nivelActual = 0
 
     def start():
-        MetodosComunes.reproducirSonido(Sonidos.HIMNO_URSS, 0)
-        MetodosComunes.reproducirSonido(Sonidos.JIJIJIJA, 1)
+        pass
 
     def stop():
-        MetodosComunes.detenerSonidos()
+        detenerSonidos()
 
     def restart():
         pass
@@ -23,13 +41,3 @@ class Sistema:
     def siguienteNivel(self):
         if self.nivelActual != len(self.niveles) - 1:
             self.nivelActual += 1
-    
-while 1:
-
-    sistema = Sistema()
-    sistema.start()
-    MetodosComunes.delay(4)
-    sistema.stop()
-    #nivel0
-    #nivle1
-    #nivle2
