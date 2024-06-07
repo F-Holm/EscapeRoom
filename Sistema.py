c8cad19d17cfa3e733bb993724dca76a2ae66972
a <a@a.com>
2024-06-07 09:09:54 -0300

    Agregamos la comunicación con el juegoIra

--- Sistema.py
+++ Sistema.py
@@ -4,6 +4,7 @@ from time import sleep
 import tkinter as tk
 from tkinter import messagebox, ttk
 from tkinter import *
+import serial, time
 
 class Sonidos(Enum):
     HIMNO_URSS = "Sonidos/Himno nacional de la Unión de Repúblicas Socialistas Soviéticas.mp3"
@@ -18,8 +19,8 @@ def reproducirSonido(sonido, loop, canal):#enum sonido, bool loop, int canal
     else:
         pygame.mixer.Channel().play(pygame.mixer.Sound(sonido.value), -1)
 
-def detenerSonidos():
-    pygame.mixer.music.stop()
+def detenerSonido(canal):
+    pygame.mixer.Channel(canal).stop()
 
 def delay(segundos):
     sleep(segundos)
@@ -29,28 +30,57 @@ sistema = Sistema()
 root = tk.Tk()
 root.title("Escape room")
 
-juegoAnterior = ttk.Button(root, text="Botón 1 (A)", command=sistema.nivelAnterior)
+juegoAnterior = ttk.Button(root, text="Nivel anterior", command=sistema.nivelAnterior)
 juegoAnterior.grid(row=0, column=0, padx=10, pady=10)
 root.bind("<KeyPress-Left>", lambda e: sistema.nivelAnterior())
 
-juegoSiguiente = ttk.Button(root, text="Botón 2 (B)", command=sistema.nivelAnterior)
+juegoSiguiente = ttk.Button(root, text="Siguiente nivel", command=sistema.nivelAnterior)
 juegoSiguiente.grid(row=0, column=1, padx=10, pady=10)
 root.bind("<KeyPress-Right>", lambda e: sistema.nivelAnterior())
 
-def siguienteJuego
+juegoSiguiente = ttk.Button(root, text="Iniciar nivel", command=sistema.start)
+juegoSiguiente.grid(row=0, column=1, padx=10, pady=10)
+root.bind("<KeyPress-Up>", lambda e: sistema.nivelAnterior())
+
+juegoSiguiente = ttk.Button(root, text="Detener nivel", command=sistema.stop)
+juegoSiguiente.grid(row=0, column=1, padx=10, pady=10)
+root.bind("<KeyPress-Down>", lambda e: sistema.stop())
+
+juegoSiguiente = ttk.Button(root, text="Reiniciar nivel", command=sistema.restart)
+juegoSiguiente.grid(row=0, column=1, padx=10, pady=10)
+root.bind("<Return>", lambda e: sistema.restart())
+
+juegoSiguiente = ttk.Button(root, text="Terminar juego", command=sistema.terminalJuego)
+juegoSiguiente.grid(row=0, column=1, padx=10, pady=10)
+root.bind("<Escape>", lambda e: sistema.terminalJuego())
+
+class juegoIra:
+    arduino = serial.Serial('COM3', 9600)
+
+    def start():
+        arduino.write(b'0')
+
+    def stop():
+        arduino.write(b'1')
+
+    def restart():
+        arduino.write(b'2')
+
+    def closeArduino():
+        arduino.close()
 
 class Sistema:
-    niveles = []#Agregá niveles utilizando las clases hijas -> [Nivle1(), Nivle2(), Nivel3("qwerty"), ...]
+    niveles = [juegoIra()]#Agregá niveles utilizando las clases hijas -> [Nivle1(), Nivle2(), Nivel3("qwerty"), ...]
     nivelActual = 0
 
     def start():
-        pass
+        niveles[nivelActual].start()
 
     def stop():
-        detenerSonidos()
+        niveles[nivelActual].stop()
 
     def restart():
-        pass
+        niveles[nivelActual].restart()
 
     def nivelAnterior(self):
         if self.nivelActual != 0:
@@ -59,3 +89,13 @@ class Sistema:
     def siguienteNivel(self):
         if self.nivelActual != len(self.niveles) - 1:
             self.nivelActual += 1
+        else:
+            self.terminalJuego()
+    
+    def terminalJuego(self):
+        self.stop()
+        self.niveles[0].closeArduino()
+        pygame.quit()
+        root.quit()
+        root.destroy()
+        sys.exit()
