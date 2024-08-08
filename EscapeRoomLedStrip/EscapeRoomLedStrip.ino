6450f666e9a493dbcb36d3a3cab17294ddc1f2cf
SantiagoEsparrach <santiesparrachperlo@gmail.com>
2024-08-08 08:26:44 -0300

    Actualizamos el archivo .ino

--- EscapeRoomLedStrip/EscapeRoomLedStrip.ino
+++ EscapeRoomLedStrip/EscapeRoomLedStrip.ino
@@ -4,7 +4,7 @@ const int greenPin = 5;
 const int bluePin = 6;
 
 // cantidad de juegos completados
-int juegosCompletados = 0;
+//int juegosCompletados = 0;
 
 // valor de cada color
 int red = 0;
@@ -31,28 +31,45 @@ void setup() {
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);
 
-    analogWrite(redPin, r);
-    analogWrite(greenPin, g);
-    analogWrite(bluePin, b);
+    analogWrite(redPin, red);
+    analogWrite(greenPin, green);
+    analogWrite(bluePin, blue);
 
     Serial.begin(9600);
 }
 
 void loop(){
     // FALTA RECIBIR EL INT DE HOLM
-
-    if(Serial.avaliable() > 0){
-      string colores = Serial.read();
-      if (colores.size() == 1){
-        switch (colores[0]){
-          case 0:
-            rayo();
-            cambiarColor(red, green, blue);
-            break;
+    /*cambiarColor(255,0,0);
+    delay(500);
+    cambiarColor(255,255,0);
+    delay(500);
+    cambiarColor(0,255,0);
+    delay(500);
+    cambiarColor(0,255,255);
+    delay(500);
+    cambiarColor(0,0,255);
+    delay(500);
+    cambiarColor(255,0,255);
+    delay(500);*/
+if (Serial.available() > 0) {
+    String colores = Serial.readString(); // Cambiar a readString
+    if (colores.length() == 1) { // Usar length() para verificar el tamaño
+        switch (colores[0]) {
+            case '0': // Comparar como carácter
+                rayo();
+                cambiarColor(red, green, blue); // Asegúrate de que estas variables estén definidas
+                break;
+            // Puedes añadir más casos aquí si es necesario
         }
-      }
-      else cambiarColor(colores[0], colores[1], colores[2]);
+    } else if (colores.length() >= 3) { // Asegúrate de que hay al menos 3 caracteres para RGB
+        int r = colores[0];
+        int g = colores[1];
+        int b = colores[2];
+        cambiarColor(r, g, b);
     }
+}
+
     /*
     // 5 presets, 1 por cada etapa. Desde rojo hasta blanco
     switch(juegosCompletados) {
