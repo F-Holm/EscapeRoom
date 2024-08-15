7e9a46678adca0d59af378076972a239c2277933
Tu Nombre <you@example.com>
2024-08-15 15:20:13 -0300

    Creamos la branch y subimos el código de prueba

--- Codigos.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class Codigos(Enum):
-    START = chr(0)
-    RESTART = chr(1)
-    STOP = chr(2)
-    CLOSE = chr(3)
-    TERMINO = chr(4)
--- Puertos.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class Puertos(Enum):
-    IRA = '/dev/ttyUSB0'
-    LEDS_RGB = '/dev/ttyUSB0'
-    IP_TRIVIA = '192.168.0.2'
-    PUERTO_TRIVIA = 8080
