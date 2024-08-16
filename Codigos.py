a9a42d23d570363943adeeefa9978ec73a42299d
SantiagoEsparrach <santiesparrachperlo@gmail.com>
2024-08-16 08:31:59 -0300

    Actualizamos .gitignore

--- Codigos.py
+++ Codigos.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 class Codigos(Enum):
-    START = b'\x00'# Inicia el juego
+    START = b'\x00'  # Inicia el juego
     RESTART = b'\x01'# Reinicia el juego
-    STOP = b'\x02'# Detiene el juego pero puede volver a iniciarse
-    CLOSE = b'\x03'# Detiene el juego y no puede volver a iniciarse
+    STOP = b'\x02'   # Detiene el juego pero puede volver a iniciarse
+    CLOSE = b'\x03'  # Detiene el juego y no puede volver a iniciarse
     TERMINO = b'\x04'# Indica que el juego terminó. Esto se mande desde el juego al sistema
