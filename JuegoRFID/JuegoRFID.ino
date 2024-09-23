b8c0186a64b3979ef00e0fbd7b6ee8b6f70022af
F-Holm <101185761+F-Holm@users.noreply.github.com>
2024-09-23 12:04:41 -0300

    Funciona. Falta agregar parpadeo al boton

--- JuegoRFID/JuegoRFID.ino
+++ JuegoRFID/JuegoRFID.ino
@@ -9,8 +9,8 @@
 
 #define NR_OF_READERS 2
 
-#define BOTON 3
-#define LED_BOTON 2
+#define boton 3
+#define ledBoton 2
 
 byte ssPins[] = {SS_1_PIN, SS_2_PIN};
  
@@ -20,208 +20,136 @@ MFRC522 mfrc522[NR_OF_READERS];
 String tags[]={"",""};
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); //nos permite controlar la tira con funciones de la libreria neopixel
 
+// Define las parejas de tags aquí
+const String parejas[5][2] = {
+  {"b3958b17", "a052f279"}, // Pareja 1
+  {"c32af216", "18b061c9"}, // Pareja 2
+  {"a3f53695", "a3997b95"}, // Pareja 3
+  {"a3fe7f95", "d3e466a8"}, // Pareja 4
+  {"a39449a8", "43863495"} // Pareja 5
+};
+
 enum Codigos {
   RFID_START = 0,
   RFID_RESTART = 1,
   RFID_STOP = 2,
   CLOSE = 3,
   RFID_TERMINO = 4,
-
   BOTON_START = 160,
   BOTON_RESTART = 161,
   BOTON_STOP = 162,
   BOTON_TERMINO = 163,
-
-  PAREJAS_BASE = 192//192 + cantParejasCorrectas = código
+  PAREJAS_0 = 192,
+  PAREJAS_1 = 193,
+  PAREJAS_2 = 194,
+  PAREJAS_3 = 195,
+  PAREJAS_4 = 196
 };
 
-struct Boton{
-  int anterior;
-  bool encendido;
-  int intervalo = 400;
-  bool iniciado;
+bool estadoParejas[NUMPIXELS] = {false, false, false, false, false}; // Estado de cada pareja (si fue ingresada o no)
+int contadorParejasCorrectas = 0; // Contador de parejas correctas
 
-  void setParpadear(){
-    anterior = millis();
-    encendido = true;
-    digitalWrite(LED_BOTON, HIGH);
-  }
+bool juegoBotonIniciado = false;
+bool juegoRFIDIniciado = false;
 
-  bool debeParpadear(){
-    return (millis() - anterior > intervalo && iniciado);
-  }
+unsigned long int anterior = 0;
+bool encendido = false;
+int cantParpadeos = 0;
+int r;
+int g;
+int b;
 
-  void parpadear(){
-    if (!debeParpadear()) return;
-    anterior = millis();
-    if (encendido)
-    {
-      encendido = false;
-      digitalWrite(LED_BOTON, LOW);
-    }
-    else{
-      encendido = true;
-      digitalWrite(LED_BOTON, HIGH);
-    }
-  }
-
-  void termino(){
-    Serial.print(Codigos::BOTON_TERMINO);
-    iniciado = false;
-    digitalWrite(LED_BOTON, LOW);
-  }
-
-} boton;
-
-struct RFID{
-  int anterior;
-  bool encendido;
-  int cantParpadeos;
-  int r;
-  int g;
-  int b;
-  int intervalo = 400;
-  bool iniciado;
-
-  bool estadoParejas[NUMPIXELS] = {false, false, false, false, false}; // Estado de cada pareja (si fue ingresada o no)
-  int contadorParejasCorrectas = 0; // Contador de parejas correctas
-
-  // Define las parejas de tags aquí
-  const String parejas[5][2] = {
-  {"b3958b17", "a052f279"}, // Pareja 1
-  {"c32af216", "18b061c9"}, // Pareja 2
-  {"98765432", "fedcba98"}, // Pareja 3
-  {"87654321", "1234abcd"}, // Pareja 4
-  {"11223344", "55667788"} // Pareja 5
-  };
-
-  void setVariables(){
-    for (int i = 0;i < NUMPIXELS;i++) estadoParejas[i] = false;
-    contadorParejasCorrectas = 0;
-    terminarParpadeo();
-  }
-
-  void setParpadear(int _cant, int _r, int _g, int _b){
-    anterior = millis();
-    cantParpadeos = _cant;
-    r = _r;
-    g = _g;
-    b = _b;
-    encendido = true;
-    cambiarColorUniforme(r, g, b);
-  }
-
-  bool debeParpadear(){
-    return (millis() - anterior > intervalo || cantParpadeos > 0) && iniciado;
-  }
-
-  void parpadear(){
-    if (!debeParpadear()) return;
-    anterior = millis();
-    if (encendido)
-    {
-      encendido = false;
-      cambiarColorUniforme(0, 0, 0);
-      cantParpadeos--;
-      if(cantParpadeos == 0){
-        setRespuestasCorrectasNeopixel();
-      }
-    }
-    else{
-      encendido = true;
-      cambiarColorUniforme(r, g, b);
-    }
-  }
-
-  terminarParpadeo(){
-    cantParpadeos = 0;
-    encendido = false;
-    cambiarColorUniforme(0, 0, 0);
-  }
-
-  void termino(){
-    Serial.print(Codigos::RFID_TERMINO);
-    iniciado = false;
-  }
-
-  void cambiarColorUniforme(int _r, int _g, int _b){
-    for(int i = 0; i < NUMPIXELS; i++){ // apago todo los neopixel
-      pixels.setPixelColor(i, _r, _g, _b);
-    }
-    pixels.show();
-  }
-
-  void setRespuestasCorrectasNeopixel(){
-    rfid.cantParpadeos = 0;
-    for (int i = 0; i < NUMPIXELS; i++){
-      if (i < contadorParejasCorrectas) pixels.setPixelColor(i, pixels.Color(0, 255, 0));
-      else pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-
-    }
-    pixels.show();
-  }
-
-  void verificarCombinacion() {
-    for (int i = 0; i < NUMPIXELS; i++) {
-    // Comprobar si los tags leídos corresponden a una pareja y si no han sido utilizados
-      if ((tags[0] == parejas[i][0] && tags[1] == parejas[i][1]) || (tags[0] == parejas[i][1] && tags[1] == parejas[i][0])) {
-        if (!estadoParejas[i]) { // Si la pareja no ha sido ingresada
-          estadoParejas[i] = true; // Marcamos la pareja como ingresada
-          contadorParejasCorrectas++; // Aumentamos el contador
-          // Encender el LED correspondiente
-
-          setRespuestasCorrectasNeopixel();
-
-          // Reseteamos las variables después de una combinación correcta
-          tags[0] = "";
-          tags[1] = "";
-          return; // Salimos de la función
-        } else { // si la pareja ya se ingresó
-          setParpadear(2, 255, 96, 0);
-        }
-      }
-    }
-    // Si se leyeron ambos tags pero no corresponden a una pareja válida
-    if (tags[0] != "" && tags[1] != "") {
-      setParpadear(2, 255, 0, 0);
-      // probar si aca hace falta que vuelva a poner en verde o se ponen solos
-      // Reseteamos las variables después de una combinación incorrecta
-      tags[0] = "";
-      tags[1] = "";
-    }
-  }
-
-} rfid;
+void setParpadear(int _cant, int _r, int _g, int _b){
+  anterior = millis();
+  cantParpadeos = _cant;
+  r = _r;
+  g = _g;
+  b = _b;
+}
 
 void recibirDatos(){
   if (Serial.available() > 0){
     int info = Serial.read();
     switch (info){
       case Codigos::RFID_START://iniciar
-        rfid.iniciado = true;
+        juegoRFIDIniciado = true;
         setVariables();
         break;
       case Codigos::RFID_RESTART://reiniciar
         setVariables();
         break;
       case Codigos::RFID_STOP://terminar
-        rfid.iniciado = false;
+        juegoRFIDIniciado = false;
         break;
       case Codigos::BOTON_START://iniciar
-        boton.iniciado = true;
+        juegoBotonIniciado = true;
         break;
       case Codigos::BOTON_RESTART://reiniciar
         break;
       case Codigos::BOTON_STOP://detener
-        boton.iniciado = false;
+        juegoBotonIniciado = false;
         break;
     }
   }
 }
 
+void terminoBoton(){
+  Serial.print(Codigos::BOTON_TERMINO);
+  juegoBotonIniciado = false;
+  digitalWrite(ledBoton, LOW);
+}
+
+void terminoRFID(){
+  Serial.print(Codigos::RFID_TERMINO);
+  juegoRFIDIniciado = false;
+}
+
+void setVariables(){
+  for (int i = 0;i < NUMPIXELS;i++) estadoParejas[i] = false;
+  contadorParejasCorrectas = 0;
+  cantParpadeos = 0;
+}
+
+void cambiarColorUniforme(int _r, int _g, int _b){
+  for(int i = 0; i < NUMPIXELS; i++){ // apago todo los neopixel
+    pixels.setPixelColor(i, _r, _g, _b);
+  }
+  pixels.show();
+}
+
+void setRespuestasCorrectasNeopixel(){
+  cantParpadeos = 0;
+  for (int i = 0; i < NUMPIXELS; i++){
+    if (i < contadorParejasCorrectas) pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+    else pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+
+  }
+  pixels.show();
+}
+
+// Función para parpadear los LEDs en rojo
+void parpadear() {
+  if(cantParpadeos == 0) return;
+  if(millis() - anterior < 400) return;
+  anterior = millis();
+  if (encendido)
+  {
+    encendido = false;
+    cambiarColorUniforme(0, 0, 0);
+    cantParpadeos--;
+    if(cantParpadeos == 0){
+      setRespuestasCorrectasNeopixel();
+    }
+  }
+  else{
+    encendido = true;
+    cambiarColorUniforme(r, g, b);
+  }
+}
+
 void setup() {
-  pinMode(BOTON, INPUT_PULLUP);//No se si es INPUT o INPUT_PULLUP
-  pinMode(LED_BOTON, OUTPUT);
+  pinMode(boton, INPUT_PULLUP);//No se si es INPUT o INPUT_PULLUP
+  pinMode(ledBoton, OUTPUT);
   
   Serial.begin(9600); // Initialize serial communications with the PC
   //while (!Serial); // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
@@ -233,39 +161,65 @@ void setup() {
     mfrc522[reader].PCD_DumpVersionToSerial();
   }
 
-  pixels.begin();
+  
 
-  rfid.cambiarColorUniforme(0, 0, 0);
+  pixels.begin();
+  //pruebaLEDs();
+  cambiarColorUniforme(0, 0, 0);
+}
+ 
+void verificarCombinacion() {
+  for (int i = 0; i < NUMPIXELS; i++) {
+  // Comprobar si los tags leídos corresponden a una pareja y si no han sido utilizados
+    if ((tags[0] == parejas[i][0] && tags[1] == parejas[i][1]) || (tags[0] == parejas[i][1] && tags[1] == parejas[i][0])) {
+      if (!estadoParejas[i]) { // Si la pareja no ha sido ingresada
+        estadoParejas[i] = true; // Marcamos la pareja como ingresada
+        contadorParejasCorrectas++; // Aumentamos el contador
+        // Encender el LED correspondiente
+        
+        setRespuestasCorrectasNeopixel();
+
+        // Reseteamos las variables después de una combinación correcta
+        tags[0] = "";
+        tags[1] = "";
+        return; // Salimos de la función
+      } else { // si la pareja ya se ingresó
+        setParpadear(2, 255, 96, 0);
+      }
+    }
+  }
+  // Si se leyeron ambos tags pero no corresponden a una pareja válida
+  if (tags[0] != "" && tags[1] != "") {
+    setParpadear(2, 255, 0, 0);
+    // probar si aca hace falta que vuelva a poner en verde o se ponen solos
+    // Reseteamos las variables después de una combinación incorrecta
+    tags[0] = "";
+    tags[1] = "";
+  }
 }
 
-
 void loop() {
+  parpadear();
   recibirDatos();
-  //rfid.iniciado = true;
+  //juegoRFIDIniciado = true;
 
-  if (rfid.iniciado){
-    rfid.parpadear();
-    Serial.print(rfid.contadorParejasCorrectas + Codigos::PAREJAS_BASE);
-
+  if (juegoRFIDIniciado){
     for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
-
       if (mfrc522[reader].PICC_IsNewCardPresent() && mfrc522[reader].PICC_ReadCardSerial()) {
         tags[reader]="";
-
         for (byte i = 0; i < mfrc522[reader].uid.size; i++) {
           tags[reader] += String(mfrc522[reader].uid.uidByte[i] < 0x10 ? "0" : "");
           tags[reader] += String(mfrc522[reader].uid.uidByte[i], HEX);
         }
-
         mfrc522[reader].PICC_HaltA();
         mfrc522[reader].PCD_StopCrypto1();
       }
     }
     verificarCombinacion();
-    if (rfid.contadorParejasCorrectas == NUMPIXELS) rfid.termino();
-  } else if (boton.iniciado && digitalRead(BOTON)){
-    boton.termino();
-  } else if (boton.iniciado){
-    boton.parpadear();
+    if (contadorParejasCorrectas == NUMPIXELS) terminoRFID();
+  } else if (juegoBotonIniciado && !digitalRead(boton)){
+    terminoBoton();
+  } else if (juegoBotonIniciado){
+    digitalWrite(ledBoton, HIGH);
   }
 }
