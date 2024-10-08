1a43de6c029460b466f6de0f29ac38da650001af
Tu Nombre <you@example.com>
2024-10-08 15:39:48 -0300

    cambiamos de librería

--- Leds/Leds.ino
+++ Leds/Leds.ino
@@ -1,26 +1,25 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
+FASTLED_USING_NAMESPACE
 
-#define PIN_1 11
-#define PIN_2 9
-#define PIN_3 10
-#define NUMPIXELS_1 300
-#define NUMPIXELS_2 208
-#define NUMPIXELS_3 92
+#define LED_TYPE    WS2812
+#define COLOR_ORDER GRB
+#define NUM_LEDS    300
+CRGB leds[300];
+
+#define BRIGHTNESS          255
+#define FRAMES_PER_SECOND  200
 
 #define RED 3
 #define GREEN 5
 #define BLUE 6
 
-Adafruit_NeoPixel pixels_1;
-Adafruit_NeoPixel pixels_2;
-Adafruit_NeoPixel pixels_3;
 
 unsigned int brilloNeoPixel = 50;
 
 String color = String('\0') + String('\0') + String('\0');
 
 unsigned long previousMillis = 0;
-char efectoActual = "";
+int efectoActual = -1;
 int etapaActual = -1;
 bool efectoActivo = false;
 
@@ -52,96 +51,7 @@ class RGB{
     }
 };
 
-class NEOPIXEL{
-  public:
-    static void cambiarColorUniforme(int _r, int _g, int _b){
-      cambiarColorUniforme1(_r, _g, _b);
-      cambiarColorUniforme2(_r, _g, _b);
-      cambiarColorUniforme3(_r, _g, _b);
-    }
-
-    static void cambiarColorUniforme1(int _r, int _g, int _b){
-      for(int i = 0; i < NUMPIXELS_1; i++){
-        pixels_1.setPixelColor(i, _r, _g, _b);
-      }
-      pixels_1.show();
-    }
-
-    static void cambiarColorUniforme2(int _r, int _g, int _b){
-      for(int i = 0; i < NUMPIXELS_2; i++){
-        pixels_2.setPixelColor(i, _r, _b, _g);
-      }
-      pixels_2.show();
-    }
-
-    static void cambiarColorUniforme3(int _r, int _g, int _b){
-      for(int i = 0; i < NUMPIXELS_3; i++){
-        pixels_3.setPixelColor(i, _r, _b, _g);
-      }
-      pixels_3.show();
-    }
-
-    static void defalutNeoPixel(){
-      cambiarColorUniforme(0, 0 , 0);
-    }
-    
-    static void cambiarColorCantidad1(int pinInicial, int cantidad, int _r, int _g, int _b){
-      for(int i = pinInicial; i < pinInicial+cantidad && i < NUMPIXELS_1; i++){
-        pixels_1.setPixelColor(i, _r, _b, _g);
-      }
-      if (pinInicial+cantidad < NUMPIXELS_1){
-        unsigned int p = pinInicial + cantidad - NUMPIXELS_1;
-        for(int i = pinInicial; i <= p && i < NUMPIXELS_1; i++){
-          pixels_1.setPixelColor(i, _r, _b, _g);
-        }
-      }
-      pixels_1.show();
-    }
-    static void cambiarColorCantidad2(int pinInicial, int cantidad, int _r, int _g, int _b){
-      for(int i = pinInicial; i < pinInicial+cantidad && i < NUMPIXELS_2; i++){
-        pixels_2.setPixelColor(i, _r, _b, _g);
-      }
-      if (pinInicial+cantidad < NUMPIXELS_1){
-        unsigned int p = pinInicial + cantidad - NUMPIXELS_2;
-        for(int i = pinInicial; i <= p && i < NUMPIXELS_2; i++){
-          pixels_2.setPixelColor(i, _r, _b, _g);
-        }
-      }
-    }
-    static void cambiarColorCantidad3(int pinInicial, int cantidad, int _r, int _g, int _b){
-      for(int i = pinInicial; i < pinInicial+cantidad && i < NUMPIXELS_3; i++){
-        pixels_3.setPixelColor(i, _r, _b, _g);
-      }
-      if (pinInicial+cantidad < NUMPIXELS_1){
-        unsigned int p = pinInicial + cantidad - NUMPIXELS_3;
-        for(int i = pinInicial; i <= p && i < NUMPIXELS_3; i++){
-          pixels_3.setPixelColor(i, _r, _b, _g);
-        }
-      }
-    }
-
-    static unsigned int i1;
-    static unsigned int i2;
-    static unsigned int i3;
-
-    static void rojoVariado1(){
-      int segmento=30;
-      i1 = 0;
-      i2 = 0;
-      i3 = 0;
-      while(true){
-        cambiarColorUniforme(180, 0, 0);
-        i1++;
-        i2++;
-        i3++;
-        cambiarColorCantidad1(i1, 20, 255, 0, 0);
-        cambiarColorCantidad2(i2, 20, 255, 0, 0);
-        cambiarColorCantidad3(i3, 20, 255, 0, 0);
-      }
-    }
-};
-
-void setEfecto(char e){
+void setEfecto(int e){
   efectoActual = e;
   efectoActivo = true;
   etapaActual = 0;
@@ -150,13 +60,94 @@ void setEfecto(char e){
 
 void efecto(){
   switch (efectoActual){
+    case 0:
+      confetti();
+      break;
+    case 1:
+      lightning();
+      break;
+  }
+}
+
+//NEOPIXEL FASTLED
+void rayito() 
+{
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  for (int veces=0;veces<random(0,15);veces++)
+  {
+    int pos = random16(NUM_LEDS);
+    leds[pos] += CRGB( random(200,255), random(200,255), random(200,255));
+
+  }
+  FastLED.show(); 
+}
+
+void confetti() 
+{
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  for (int veces=0;veces<random(0,15);veces++)
+  {
+    int pos = random16(NUM_LEDS);
+    leds[pos] += CRGB( 255, random(0,60), 0);
+
+  }
+  FastLED.show(); 
+}
+void lightning(){
+  unsigned int tiempo=millis();
+
+  switch (etapaActual){
+    case 0:
+      for(int i=0; i<NUM_LEDS;i++){
+        leds[i]=CRGB(255, 255, 255);
+      }
+      FastLED.show();
+      etapaActual++;
+      previousMillis = tiempo;
+      break;
+    case 1:
+      if(tiempo - 5000 >= previousMillis){
+        for(int i=0; i<NUM_LEDS;i++){
+          leds[i]=CRGB(0, 0, 0);
+        }
+        FastLED.show(); 
+        previousMillis = tiempo;
+        etapaActual++;
+      }
+      break;
+    case 2:
+      if(tiempo - 5000 >= previousMillis){
+        for(int i=0; i<NUM_LEDS;i++){
+          leds[i]=CRGB(255, 255, 255);
+        }
+        FastLED.show(); 
+        previousMillis = tiempo;
+        etapaActual++;
+      }
+      break;
+    case 3:
+      if(tiempo - 500 >= previousMillis){
+        for(int i=0; i<NUM_LEDS;i++){
+          leds[i]=CRGB(0, 0, 0);
+        }
+        FastLED.show(); 
+        previousMillis = tiempo;
+        etapaActual++;
+      }
+      break;
+    case 4:
+      setEfecto(0);//default
+      break;
   }
 }
 
 void setup() {
-  pixels_1 = Adafruit_NeoPixel(NUMPIXELS_1, PIN_1, NEO_GRB + NEO_KHZ800);
-  pixels_2 = Adafruit_NeoPixel(NUMPIXELS_2, PIN_2, NEO_GRB + NEO_KHZ800);
-  pixels_3 = Adafruit_NeoPixel(NUMPIXELS_3, PIN_3, NEO_GRB + NEO_KHZ800);
+  FastLED.addLeds<LED_TYPE,11,BRG>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, 9, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );  
+  FastLED.addLeds<LED_TYPE, 10, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+
+  FastLED.setBrightness(BRIGHTNESS);
+
 
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
@@ -164,13 +155,7 @@ void setup() {
 
   RGB::defaultColor();
 
-  pixels_1.begin();
-  pixels_1.setBrightness(brilloNeoPixel);
-  pixels_2.begin();
-  pixels_2.setBrightness(brilloNeoPixel);
-  pixels_3.begin();
-  pixels_3.setBrightness(brilloNeoPixel);
-  NEOPIXEL::cambiarColorUniforme(0, 0, 0);
+
 
   Serial.begin(9600);
 
@@ -180,6 +165,7 @@ void setup() {
     if (identificate == 5) Serial.print(8);
     else continue;
   }*/
+  setEfecto(1);
 }
 
 void loop() {
@@ -189,6 +175,6 @@ void loop() {
     if (colores.length() == 1) setEfecto(colores[0]);
     if (colores.length() == 3) cambiarColorRGB(colores);
   }*/
-  NEOPIXEL::cambiarColorUniforme(255, 0, 0);
+  efecto();
   RGB::cambiarColor(255, 0, 0);
 }
