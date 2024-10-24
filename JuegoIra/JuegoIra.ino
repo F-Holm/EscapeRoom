56901fa0deef3e3fd35a9e88b7d7516f38b5233f
Tu Nombre <you@example.com>
2024-10-24 08:32:24 -0300

    funciona

--- JuegoIra/JuegoIra.ino
+++ JuegoIra/JuegoIra.ino
@@ -7,8 +7,8 @@
 #define BOTON_GANAR_1 5
 #define BOTON_GANAR_2 6
 
-#define BOTON_PERDER_1 8
-#define BOTON_PERDER_2 9
+#define BOTON_PERDER_1 9
+#define BOTON_PERDER_2 8
 
 #define PIN_NEOPIXEL 10
 #define CANT_PIXELES 2
@@ -16,15 +16,9 @@
 bool estadoJugador1 = false;
 bool estadoJugador2 = false;
 
+bool yaEmpezo = false;
+
 Adafruit_NeoPixel pixel = Adafruit_NeoPixel(CANT_PIXELES, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
-
-unsigned int cantToques = 0;//cuantas veces tocó el alambre sin perder
-const unsigned int cantVidas = 5;//Puede tocar n veces los alambres sin perder
-const unsigned int msGodMode = 200;//Cuantos milisegundos tenés de inmunidad después de tocar un alambre
-unsigned int ultToque = 0;
-
-const unsigned int msDelayEntreNotificaciones = 1000;
-unsigned int tiempoAnteriorNotificacion = millis();
 
 bool listoEmpezar = false;
 
@@ -33,6 +27,11 @@ unsigned int tiempoLedGanar = 0;
 
 bool perdio = false;
 
+unsigned int ultToque = 0;
+const unsigned int vidas = 5;
+unsigned int vidasRestantes = vidas;
+const unsigned int msGodMode = 100;
+unsigned long int antirrebote=0;
 enum Codigos {
   START = 0,
   RESTART = 1,
@@ -49,7 +48,7 @@ enum Codigos {
 
 void setup()
 {
-  //listoEmpezar = true; perdio = true;//Para tests
+  //setVariables();//Para tests
   Serial.begin(9600);
 
   pinMode(BOTON_EMPEZAR_1, INPUT_PULLUP);
@@ -68,6 +67,12 @@ void setup()
     if (identificate == Codigos::IDENTIFICATE) { Serial.print(Codigos::ID); break; }
     else continue;
   }*/
+}
+
+void setVariables(){
+  listoEmpezar = true;
+  perdio = true;
+  ultToque = millis();
 }
 
 void setLed1(int r, int g, int b){
@@ -106,12 +111,11 @@ void recibirInfo(){
     int info = Serial.read();
     switch (info){
       case Codigos::START://iniciar
-        listoEmpezar = true;
-        perdio = true;
+        setVariables();
         break;
       case Codigos::RESTART://reiniciar
-        terminarJuego();
-        listoEmpezar = true;
+        perder();
+        setVariables();
         break;
       case Codigos::STOP://terminar
         terminarJuego();
@@ -124,30 +128,28 @@ void empezar(){
   ledGanar = false;
   setLeds(0, 0, 255);
   perdio = false;
+  Serial.print(Codigos::JUGANDO);
 }
 
 void perder(){
-  if (millis() - ultToque > msGodMode) {
-    if (cantToques == cantVidas) {
-      perdio = true;
-      estadoJugador1 = false;
-      estadoJugador2 = false;
-      cantToques = 0;
-    } else {
-      cantToques++;
-      ultToque = millis();
-    }
-  }
+  perdio = true;
+  estadoJugador1 = false;
+  estadoJugador2 = false;
+  Serial.print(Codigos::PERDIERON);
+  ultToque = millis();
+  vidasRestantes = vidas;
 }
 
 void ganarJ1(){
   estadoJugador1 = true;
   setLed1(0, 255, 0);
+  Serial.print(Codigos::TERMINO_J1);
 }
 
 void ganarJ2(){
   estadoJugador2 = true;
   setLed2(0, 255, 0);
+  Serial.print(Codigos::TERMINO_J2);
 }
 
 void ganar(){
@@ -169,7 +171,8 @@ void loop()
     perdio = true;
   }
 
-  if (listoEmpezar){
+  if (listoEmpezar && millis()-antirrebote>100){
+    antirrebote=millis();
     bool botonEmpezarState1 = !digitalRead(BOTON_EMPEZAR_1);
     bool botonPerderState1 = !digitalRead(BOTON_PERDER_1);
     bool botonPerderState2 = !digitalRead(BOTON_PERDER_2);
@@ -177,11 +180,30 @@ void loop()
     bool botonEmpezarState2 = !digitalRead(BOTON_EMPEZAR_2);
     bool botonGanarState2 = !digitalRead(BOTON_GANAR_2);
 
-    if (botonEmpezarState1 && botonEmpezarState2){
+
+    if (botonEmpezarState1 && botonEmpezarState2 && perdio){
       empezar();
     }
 
-    if (botonPerderState1 && !estadoJugador1 || botonPerderState2 && !estadoJugador2 && !perdio){
+    if (perdio && botonEmpezarState1 && !botonEmpezarState2){
+      setLed1(0, 0, 255);
+      setLed2(255, 0, 0);
+      
+    } else if (perdio && !botonEmpezarState1 && botonEmpezarState2){
+      setLed2(0, 0, 255);
+      setLed1(255, 0, 0);
+      
+    } else if (perdio && !botonEmpezarState1 && !botonEmpezarState2) {
+      setLeds(255, 0, 0);
+      
+    }
+
+    if ((botonPerderState1 && !estadoJugador1 || botonPerderState2 && !estadoJugador2) && !perdio/* && millis() <= msGodMode + ultToque*/){
+      /*if (vidasRestantes == 1) perder();
+      else {
+        vidasRestantes--;
+        ultToque = millis();
+      }*/
       perder();
     }
 
@@ -196,32 +218,13 @@ void loop()
     if (estadoJugador1 && estadoJugador2){
       ganar();
     }
-    else if (millis() - msDelayEntreNotificaciones >= tiempoAnteriorNotificacion){
-      tiempoAnteriorNotificacion = millis();
-      if (perdio) Serial.write(Codigos::PERDIERON);
-      else if (estadoJugador1) Serial.write(Codigos::TERMINO_J1);
-      else if (estadoJugador2) Serial.write(Codigos::TERMINO_J2);
-      else Serial.write(Codigos::JUGANDO);
-    }
-
-    if (perdio && botonEmpezarState1 && !botonEmpezarState2){
-      setLed1(0, 0, 255);
-      setLed2(255, 0, 0);
-    } else if (perdio && !botonEmpezarState1 && botonEmpezarState2){
-      setLed2(0, 0, 255);
-      setLed1(255, 0, 0);
-    } else if (perdio && !botonEmpezarState1 && !botonEmpezarState2) {
-      setLeds(255, 0, 0);
-    }
   }
 
-  // Tests //
-  /*
-  if (!digitalRead(BOTON_EMPEZAR_1)) Serial.println("Empezó 1");
+  //Tests
+  /*if (!digitalRead(BOTON_EMPEZAR_1)) Serial.println("Empezó 1");
   if (!digitalRead(BOTON_PERDER_1)) Serial.println("Perdió 1");
   if (!digitalRead(BOTON_PERDER_2)) Serial.println("Perdió 2");
   if (!digitalRead(BOTON_GANAR_1)) Serial.println("Ganó 1");
   if (!digitalRead(BOTON_EMPEZAR_2)) Serial.println("Empezó 2");
-  if (!digitalRead(BOTON_GANAR_2)) Serial.println("Ganó 2");
-  */
+  if (!digitalRead(BOTON_GANAR_2)) Serial.println("Ganó 2");*/
 }
